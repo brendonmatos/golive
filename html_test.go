4f4ed8085a3634d4e398175601cf83a57e938c78
brendonmatos <brendonferreiradm@gmail.com>
2020-12-31 03:40:24 -0300

    Revert "Index path node selector"

--- html_test.go
+++ html_test.go
@@ -1,7 +1,6 @@
 package golive
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -24,34 +23,33 @@ func TestCreateDOMFromString(t *testing.T) {
 }
 
 func TestSelectorFromNode(t *testing.T) {
-	html := `<div go-live-component-id><h1>Hello world<span>a</span></h1></div>`
+	html := `<body><h1>Hello world<span>a</span></h1></body>`
 
 	dom, _ := CreateDOMFromString(html)
 
-	node := dom.LastChild.LastChild.LastChild.LastChild.LastChild.FirstChild
+	node := dom.LastChild.LastChild.LastChild.LastChild.FirstChild
 	if node.Data != "a" {
 		t.Error("value was not parsed correctly")
 	}
 
-	path := PathToComponentRoot(node)
-	if !reflect.DeepEqual(path, []int{0, 0, 1, 0}) {
-		t.Error("wrong selector returned", path)
+	if SelectorFromNode(node) != "html:nth-child(1) body:nth-child(2) h1:nth-child(1) span:nth-child(1)" {
+		t.Error("wrong selector returned")
 	}
 }
 
 func TestSelectorFromEmptyNode(t *testing.T) {
-	a := `<div go-live-component-id><h1>Hello world<span></span></h1></div>`
+	a := `<body><h1>Hello world<span></span></h1></body>`
 
 	dom, _ := CreateDOMFromString(a)
 
-	node := dom.LastChild.LastChild.LastChild.LastChild.LastChild
+	node := dom.LastChild.LastChild.LastChild.LastChild
 
 	if node.Data != "span" || node.FirstChild != nil {
 		t.Error("value was not parsed correctly")
 	}
 
-	path := PathToComponentRoot(node)
-	if !reflect.DeepEqual(path, []int{0, 0, 1}) {
-		t.Error("wrong selector returned", path)
+	selector := SelectorFromNode(node)
+	if selector != "html:nth-child(1) body:nth-child(2) h1:nth-child(1) span:nth-child(1)" {
+		t.Error("wrong selector returned, given", selector)
 	}
 }
