f3d3bbee59ca150aa92a448483c40231cda49af0
Brendon <brendon@onli.com.br>
2021-01-05 02:45:11 -0300

    before merge

--- component_wrapper.go
+++ component_wrapper.go
@@ -1,5 +1,7 @@
 package golive
 
+import "fmt"
+
 // LiveComponentWrapper is a struct
 type LiveComponentWrapper struct {
 	Name      string
@@ -27,6 +29,10 @@ func (l *LiveComponentWrapper) Mounted(_ *LiveComponent) {
 
 // Commit puts an boolean to the commit channel and notifies ho is listening
 func (l *LiveComponentWrapper) Commit() {
+	if !l.component.IsMounted {
+		fmt.Printf("Not mounted")
+	}
+
 	*l.lifeCycle <- ComponentLifeTimeMessage{
 		Stage:     Updated,
 		Component: l.component,
