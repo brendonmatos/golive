bcc300a8862d910c76738df62de403b99245d97f
Brendon <brendon@onli.com.br>
2021-01-07 15:06:02 -0300

    connecting elements without appending html adding some comments to the code removing lifetime chan from component_wrapper.go working

--- component_wrapper.go
+++ component_wrapper.go
@@ -3,12 +3,10 @@ package golive
 // LiveComponentWrapper is a struct
 type LiveComponentWrapper struct {
 	Name      string
-	lifeCycle *ComponentLifeCycle
 	component *LiveComponent
 }
 
 func (l *LiveComponentWrapper) Create(lc *LiveComponent) {
-	l.lifeCycle = lc.life
 	l.component = lc
 }
 
@@ -27,8 +25,5 @@ func (l *LiveComponentWrapper) Mounted(_ *LiveComponent) {
 
 // Commit puts an boolean to the commit channel and notifies ho is listening
 func (l *LiveComponentWrapper) Commit() {
-	*l.lifeCycle <- ComponentLifeTimeMessage{
-		Stage:     Updated,
-		Component: l.component,
-	}
+	l.component.Update()
 }
